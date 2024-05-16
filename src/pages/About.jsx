4247b7c54d910d26749fb6aaa28880b9944429e5
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2024-05-16 17:06:33 +0700

    first

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// About.js
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the About page. 5555 PRPRPR</p>
+      <a href="/">Home</a>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// Home.js
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the Home page! XDXDXD</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default Home;
